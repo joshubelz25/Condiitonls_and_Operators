cc8e6d0c2c1d9b400c7fc4b3dd758376ebac2f50
joshubelz25 <joshubelz25@g.crookcountyschools.org>
2022-10-24 13:48:07 -0700

    It JUST WORKS

---
+++ script.js
@@ -0,0 +1,15 @@
+var Numb = prompt("How smart is Jr Hernandez on a scale of 0 to 1") 
+
+var letter = letter
+
+if(Numb > 1){
+alert("You are 100% incorrect go to school.")
+}
+else if(Numb < 0){
+alert("your also Correct Again because you smart not him")
+}
+var letter = prompt("How dumb am i 0 out of 0")
+
+if (Numb > 0){
+alert("Correct becasuse thats the only good answer.")
+}
